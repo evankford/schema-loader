0f62d39c4b428f50032adf14588960de8a23de23
Evan Ford <evankerrickford@gmail.com>
2022-07-04 17:18:10 -0400

    Moving shopify schema loader

---
+++ index.js
@@ -0,0 +1,21 @@
+import { getOptions } from 'loader-utils';
+import { validate } from 'schema-utils';
+
+const schema = {
+  type: 'object',
+  properties: {
+    dest: {
+      type: 'string'
+    }
+  }
+}
+
+export default function(source) {
+  const options = getOptions(this);
+
+  validate(schema, options, {
+    name: "SchemaLoader"
+  })
+  console.log("Schema Loader Running!");
+  console.log(source);
+}
